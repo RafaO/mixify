7998e12e201ef74c029cd6ceeca65ce533eb4bde
RafaO <rafao1989@hotmail.com>
2024-07-27 14:01:15 +0200

    add crashlytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:ui';
+
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mixify/api_service.dart';
@@ -5,9 +8,19 @@ import 'package:mixify/auth_view.dart';
 import 'package:mixify/playlist_grid.dart';
 import 'package:mixify/token_manager.dart';
 import 'package:spotify_sdk/spotify_sdk.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 Future main() async {
   await dotenv.load(fileName: ".env");
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
   runApp(const MyApp());
 }
 
@@ -66,21 +79,26 @@ class _MyHomePageState extends State<MyHomePage> {
     String clientId = dotenv.env['spotify_client_id'] ?? '';
     if (clientId.isEmpty) {
       // handle error
+      FirebaseCrashlytics.instance.log("clientID is empty");
       return;
     }
     const redirectUri = 'mixify://callback';
     const scope =
         "playlist-read-private, user-modify-playback-state, user-read-playback-state, user-read-currently-playing";
 
-    var accessToken = await SpotifySdk.getAccessToken(
-      clientId: clientId,
-      redirectUrl: redirectUri,
-      scope: scope,
-    );
-    widget._tokenManager.tokenReceived(accessToken);
-    setState(() {
-      authenticated = true;
-    });
+    try {
+      var accessToken = await SpotifySdk.getAccessToken(
+        clientId: clientId,
+        redirectUrl: redirectUri,
+        scope: scope,
+      );
+      widget._tokenManager.tokenReceived(accessToken);
+      setState(() {
+        authenticated = true;
+      });
+    } on Exception catch (e,s) {
+      FirebaseCrashlytics.instance.recordError(e, s);
+    }
   }
 
   @override
