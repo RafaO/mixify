28cb1da212f23ee695337c624b102402920b6af7
RafaO <rafao1989@hotmail.com>
2025-01-26 19:16:17 +0100

    add 3 months to time range

--- app/lib/entities/time_range.dart
+++ app/lib/entities/time_range.dart
@@ -4,7 +4,11 @@ abstract class TimeRange {
 
   /// Factory constructors for specific time ranges
   factory TimeRange.oneMonth() => const OneMonth();
+
+  factory TimeRange.threeMonths() => const ThreeMonths();
+
   factory TimeRange.oneYear() => const OneYear();
+
   factory TimeRange.forever() => const Forever();
 
   /// Method to calculate the start date based on the range
@@ -12,6 +16,8 @@ abstract class TimeRange {
     final now = DateTime.now();
     if (this is OneMonth) {
       return now.subtract(const Duration(days: 30));
+    } else if (this is ThreeMonths) {
+      return now.subtract(const Duration(days: 90));
     } else if (this is OneYear) {
       return now.subtract(const Duration(days: 365));
     } else if (this is Forever) {
@@ -23,6 +29,10 @@ abstract class TimeRange {
 
 class OneMonth extends TimeRange {
   const OneMonth();
+}
+
+class ThreeMonths extends TimeRange {
+  const ThreeMonths();
 }
 
 class OneYear extends TimeRange {
