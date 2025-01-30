dcc7b8bf1a3a4322266ee8a1f2dfd47f169fb557
RafaO <rafao1989@hotmail.com>
2025-01-30 13:17:37 +0100

    use firebase error handling only in prod

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,7 +1,8 @@
-import 'dart:ui';
+import 'dart:developer' as developer;
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:mixify/api_service.dart';
@@ -18,9 +19,18 @@ Future main() async {
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
+  // FirebaseCrashlytics.instance.setCrashlyticsCollectionEnabled(true);
   FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
   PlatformDispatcher.instance.onError = (error, stack) {
-    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    if (kDebugMode) {
+      developer.log(
+        'general error:',
+        name: 'com.keller.mixify',
+        error: error,
+      );
+    } else {
+      FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    }
     return true;
   };
   runApp(const MyApp());
