ec20ea9d26d42cf53468d17fe16054d843ad3861
RafaO <rafao1989@hotmail.com>
2025-02-22 00:06:56 +0100

    add UI to select artists in a different tab

--- app/lib/playlist_selector.dart
+++ app/lib/playlist_selector.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:mixafy/api_service.dart';
+import 'package:mixafy/entities/artist.dart';
 import 'package:mixafy/entities/spotify_playlist.dart';
 
 class PlaylistSelector extends StatefulWidget {
-  final Function(List<SpotifyPlaylist>) onSelectedPlaylists;
   final APIService apiService;
+  final Function(List<SpotifyPlaylist>) onSelectedPlaylists;
   final List<SpotifyPlaylist> alreadySelectedPlaylists;
 
   const PlaylistSelector({
@@ -18,44 +19,65 @@ class PlaylistSelector extends StatefulWidget {
   State<PlaylistSelector> createState() => _PlaylistSelectorState();
 }
 
-class _PlaylistSelectorState extends State<PlaylistSelector> {
+class _PlaylistSelectorState extends State<PlaylistSelector>
+    with SingleTickerProviderStateMixin {
+  late TabController _tabController;
   late List<SpotifyPlaylist> playlists;
   late List<SpotifyPlaylist> filteredPlaylists;
+  late List<Artist> artists;
+  late List<Artist> filteredArtists;
   late List<SpotifyPlaylist> selectedPlaylists;
   bool isLoading = true;
-
   TextEditingController searchController = TextEditingController();
 
   @override
   void initState() {
     super.initState();
+    _tabController = TabController(length: 2, vsync: this);
     playlists = [];
     filteredPlaylists = [];
-    _fetchPlaylists();
+    artists = [];
+    filteredArtists = [];
     selectedPlaylists = List.from(widget.alreadySelectedPlaylists);
+    _tabController.addListener(() {
+      if (_tabController.indexIsChanging) {
+        _clearSearch();
+      }
+    });
+    _fetchData();
   }
 
-  Future<void> _fetchPlaylists() async {
-    List<SpotifyPlaylist> fetchedPlaylists =
-        await widget.apiService.fetchPlaylists();
+  Future<void> _fetchData() async {
+    final fetchedPlaylists = await widget.apiService.fetchPlaylists();
+    final fetchedArtists = await widget.apiService.getUserSavedArtists();
     setState(() {
       playlists = fetchedPlaylists;
       filteredPlaylists = fetchedPlaylists;
-      selectedPlaylists = List.from(widget.alreadySelectedPlaylists);
+      artists = fetchedArtists;
+      filteredArtists = fetchedArtists;
       isLoading = false;
     });
   }
 
-  void _filterPlaylists(String query) {
-    List<SpotifyPlaylist> filtered = playlists
-        .where((playlist) => playlist.name
-            .toString()
-            .toLowerCase()
-            .contains(query.toLowerCase()))
-        .toList();
+  void _filterItems(String query) {
     setState(() {
-      filteredPlaylists = filtered;
+      if (_tabController.index == 0) { // If Playlists tab is selected
+        filteredPlaylists = playlists
+            .where((playlist) =>
+            playlist.name.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      } else { // If Artists tab is selected
+        filteredArtists = artists
+            .where((artist) =>
+            artist.name.toLowerCase().contains(query.toLowerCase()))
+            .toList();
+      }
     });
+  }
+
+  void _clearSearch() {
+    searchController.clear();
+    _filterItems(''); // Reset the filter
   }
 
   void _togglePlaylistSelection(SpotifyPlaylist playlist) {
@@ -70,23 +92,9 @@ class _PlaylistSelectorState extends State<PlaylistSelector> {
 
   @override
   Widget build(BuildContext context) {
-    final onSurfaceColor = Theme.of(context).colorScheme.onSurface;
-    final hintTextColor =
-        onSurfaceColor.withOpacity(0.6); // Slightly faded for hint text
-
     return Scaffold(
       appBar: AppBar(
-        title: Row(
-          children: [
-            Image.asset(
-              'assets/Spotify_Icon_CMYK_Black.png',
-              width: 24.0,
-              height: 24.0,
-            ),
-            const SizedBox(width: 10),
-            const Text('Spotify Playlists'),
-          ],
-        ),
+        title: const Text('Select items'),
         actions: [
           IconButton(
             icon: const Icon(Icons.check),
@@ -103,93 +111,78 @@ class _PlaylistSelectorState extends State<PlaylistSelector> {
             padding: const EdgeInsets.all(8.0),
             child: TextField(
               controller: searchController,
-              onChanged: (value) {
-                _filterPlaylists(value);
-              },
+              onChanged: _filterItems,
               decoration: InputDecoration(
-                filled: true,
-                // Use surface color for the background
-                labelText: 'Search Playlists',
-                labelStyle: TextStyle(color: onSurfaceColor),
-                // Label text color
-                hintText: 'Enter playlist name...',
-                hintStyle: TextStyle(color: hintTextColor),
-                // Faded hint text
+                labelText: 'Search',
+                hintText: 'Start typing to filter',
                 prefixIcon: const Icon(Icons.search),
                 suffixIcon: searchController.text.isNotEmpty
                     ? IconButton(
-                        icon: const Icon(Icons.cancel_outlined),
-                        onPressed: () {
-                          searchController.text = "";
-                          _filterPlaylists("");
-                        },
-                      )
+                  icon: const Icon(Icons.cancel_outlined),
+                  onPressed: _clearSearch, // Clear the search
+                )
                     : null,
-                border: UnderlineInputBorder(
+                border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(30.0),
-                  borderSide: BorderSide.none,
                 ),
               ),
             ),
           ),
-          isLoading
-              ? const Center(child: CircularProgressIndicator())
-              : Expanded(
-                  child: filteredPlaylists.isEmpty
-                      ? const Center(
-                          child: Text("No playlists found"),
-                        )
-                      : ListView.builder(
-                          itemCount: filteredPlaylists.length,
-                          itemBuilder: (context, index) {
-                            final playlist = filteredPlaylists[index];
-                            final isSelected =
-                                selectedPlaylists.contains(playlist);
-                            return Card(
-                              margin: const EdgeInsets.symmetric(
-                                horizontal: 16.0,
-                                vertical: 8.0,
-                              ),
-                              shape: RoundedRectangleBorder(
-                                borderRadius: BorderRadius.circular(12.0),
-                              ),
-                              elevation: 4.0,
-                              child: SizedBox(
-                                // height: 80.0, // Fixed height for each row
-                                child: ListTile(
-                                  leading: playlist.imageUrl != null
-                                      ? CircleAvatar(
-                                          backgroundImage:
-                                              NetworkImage(playlist.imageUrl!),
-                                        )
-                                      : const CircleAvatar(
-                                          backgroundColor: Colors.grey,
-                                          child: Icon(Icons.music_note),
-                                        ),
-                                  title: Text(
-                                    playlist.name,
-                                    overflow: TextOverflow.ellipsis,
-                                    maxLines: 1,
-                                    style: const TextStyle(
-                                        fontWeight: FontWeight.bold),
-                                  ),
-                                  onTap: () {
-                                    _togglePlaylistSelection(playlist);
-                                  },
-                                  trailing: isSelected
-                                      ? const Icon(
-                                          Icons.check_circle,
-                                          color: Colors.green,
-                                        )
-                                      : const Icon(Icons.circle_outlined),
-                                ),
-                              ),
-                            );
-                          },
-                        ),
-                ),
+          TabBar(
+            controller: _tabController,
+            tabs: const [
+              Tab(text: 'Playlists'),
+              Tab(text: 'Artists'),
+            ],
+          ),
+          Expanded(
+            child: TabBarView(
+              controller: _tabController,
+              children: [
+                _buildPlaylistList(),
+                _buildArtistList(),
+              ],
+            ),
+          ),
         ],
       ),
+    );
+  }
+
+  Widget _buildPlaylistList() {
+    return isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : ListView.builder(
+      itemCount: filteredPlaylists.length,
+      itemBuilder: (context, index) {
+        final playlist = filteredPlaylists[index];
+        final isSelected = selectedPlaylists.contains(playlist);
+        return ListTile(
+          leading: playlist.imageUrl != null
+              ? CircleAvatar(
+              backgroundImage: NetworkImage(playlist.imageUrl!))
+              : const Icon(Icons.music_note),
+          title: Text(playlist.name),
+          trailing: isSelected
+              ? const Icon(Icons.check_circle, color: Colors.green)
+              : const Icon(Icons.circle_outlined),
+          onTap: () => _togglePlaylistSelection(playlist),
+        );
+      },
+    );
+  }
+
+  Widget _buildArtistList() {
+    return isLoading
+        ? const Center(child: CircularProgressIndicator())
+        : ListView.builder(
+      itemCount: filteredArtists.length,
+      itemBuilder: (context, index) {
+        final artist = filteredArtists[index];
+        return ListTile(
+          title: Text(artist.name),
+        );
+      },
     );
   }
 }
