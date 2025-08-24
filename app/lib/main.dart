e942999d5084e9471979691be48a2e80c70ebb6a
RafaO <rafao1989@hotmail.com>
2025-08-24 10:31:42 +0200

    remove unused import

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,5 +1,3 @@
-import 'dart:developer' as developer;
-
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/foundation.dart';
